88a9a765d2fdad7c8bdb896ff6bac20e344e8696
NayanDUET <nayan.duet.cse@gmail.com>
2024-12-06 01:17:44 -0500

    Add files via upload

---
+++ lab-7/strongly connected component.cpp
@@ -0,0 +1,67 @@
+//Nayan Chandra, CSE-21,ID-214001
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs1(int node, vector<int> adj[], vector<bool> &visited, stack<int> &st) {
+    visited[node] = true;
+    for (int neighbor : adj[node]) {
+        if (!visited[neighbor]) {
+            dfs1(neighbor, adj, visited, st);
+        }
+    }
+    st.push(node);
+}
+void dfs2(int node, vector<int> transposedAdj[], vector<bool> &visited, vector<int> &component) {
+    visited[node] = true;
+    component.push_back(node);
+    for (int neighbor : transposedAdj[node]) {
+        if (!visited[neighbor]) {
+            dfs2(neighbor, transposedAdj, visited, component);
+        }
+    }
+}
+void findSCCs(int V, vector<int> adj[]) {
+    stack<int> st;
+    vector<bool> visited(V, false);
+
+    for (int i = 0; i < V; i++) {
+        if (!visited[i]) {
+            dfs1(i, adj, visited, st);
+        }
+    }
+    vector<int> transposedAdj[V];
+    for (int i = 0; i < V; i++) {
+        for (int neighbor : adj[i]) {
+            transposedAdj[neighbor].push_back(i);
+        }
+    }
+
+    fill(visited.begin(), visited.end(), false);
+    cout << "Strongly Connected Components are:\n";
+    while (!st.empty()) {
+        int node = st.top();
+        st.pop();
+        if (!visited[node]) {
+            vector<int> component;
+            dfs2(node, transposedAdj, visited, component);
+            for (int v : component) {
+                cout << v << " ";
+            }
+            cout << endl;
+        }
+    }
+}
+
+int main() {
+    int V, E;
+    cout << "Enter the number of vertices and edges: ";
+    cin >> V >> E;
+    vector<int> adj[V];
+    for (int i = 0; i < E; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+    }
+    findSCCs(V, adj);
+    return 0;
+}
